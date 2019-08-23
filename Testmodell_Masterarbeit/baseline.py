32a79c652a3dfdb14484729c5411ba8fbd336c4f
Dominic <dominic.neu@port-neu.de>
2019-08-24 01:28:41 +0200

    Baseline Berechnung

--- Testmodell_Masterarbeit/baseline.py
+++ Testmodell_Masterarbeit/baseline.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from calender import get_german_holiday_calendar
+
 DATA_PATH = os.path.join('F:', 'OneDrive', 'Dokumente', '1 Universität - Master', '6. Semester', 'Masterarbeit', 'Implementation', 'Echtdaten')
 
 """ 
@@ -22,24 +24,70 @@ def reward(x):
     return reward
 
 Wareneingang = pd.read_csv(
-    os.path.join(DATA_PATH, '99 Art_WE.csv')
+    os.path.join(DATA_PATH, '0 Wareneingang.time.csv'),
+    header=0,
+    names=['Markt', 'Artikel', 'Belegtyp', 'Menge', 'Datum']
     )
-
+Wareneingang.rename(columns={'MARKT': 'Markt', 'oldName2': 'newName2'}, inplace=True)
 Warenausgang = pd.read_csv(
-    os.path.join(DATA_PATH, '99 Art_WA.csv')
+    os.path.join(DATA_PATH, '0 Warenausgang.time.csv'),
+    header=0,
+    names=['Markt', 'Artikel', 'Belegtyp', 'Menge', 'Datum']
     )
 
 Warenbestand = pd.read_csv(
-    os.path.join(DATA_PATH, '99 Art_WB.csv')
+    os.path.join(DATA_PATH, '0 Warenbestand.time.csv'),
+    header=0,
+    names=['Markt', 'Artikel', 'Bestand', 'MengeEK', 'MengeVK', 'Anfangsbestand', 'Datum']
     )
-Wareneingang['Datum'] =  pd.to_datetime(Wareneingang['Datum'], format='%Y-%m-%d')
-Warenausgang['Datum'] =  pd.to_datetime(Warenausgang['Datum'], format='%Y-%m-%d')
+Wareneingang['Datum'] =  pd.to_datetime(Wareneingang['Datum'], format='%d.%m.%y')
+Warenausgang['Datum'] =  pd.to_datetime(Warenausgang['Datum'], format='%d.%m.%y')
 
-#Warenausgang = Warenausgang[Warenausgang.Datum.dt.year == 2018]
-#Wareneingang = Wareneingang[Wareneingang.Datum.dt.year == 2018]
 
-Warenausgang = Warenausgang.set_index(Warenausgang.Datum)
-Wareneingang = Wareneingang.set_index(Wareneingang.Datum)
+
+Warenausgang = Warenausgang.set_index('Artikel')
+Wareneingang = Wareneingang.set_index('Artikel')
+Warenbestand = Warenbestand.set_index('Artikel')
+
+ausgang = Warenausgang.loc[22299].copy()
+eingang = Wareneingang.loc[22299].copy()
+bestand = Warenbestand.loc[22299].copy()
+
+eingang.Menge = -eingang.Menge
+
+bewegung = pd.concat([eingang, ausgang]).drop(columns=['Markt'])
+
+bewegung_agg = bewegung.groupby('Datum').sum()
+
+cal_cls = get_german_holiday_calendar('SL')
+cal = cal_cls()
+
+sl_bd = pd.tseries.offsets.CustomBusinessDay(calendar=cal, weekmask='Mon Tue Wed Tue Fri Sat')
+
+zeitraum = pd.date_range('2016-01-01', '2019-06-30', freq=sl_bd)
+
+bewegung_agg = bewegung_agg.reindex(zeitraum, fill_value=0)
+
+bewegung_agg.sort_index(ascending=False, inplace=True)
+
+bewegung_agg.iloc[0] += bestand.Bestand
+
+bewegung_agg['Test'] = bewegung_agg['Menge'].cumsum()
+bewegung_agg['Reward'] = bewegung_agg['Test'].apply(lambda x: reward(x))
+
+f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+bewegung_agg.plot(y='Test', title='Bestandsentwicklung', use_index=True, ax=ax1)
+bewegung_agg.plot(y='Reward', title='Belohnung {:0.2f}'.format(np.sum(bewegung_agg['Reward'])), use_index=True, ax=ax2)
+plt.show()
+
+
+
+
+
+
+
+
+
 
 alle_artikel = Warenbestand.Artikel.to_numpy()
 
