45569a58ce61451bd7850ddd1351729bf8ca57c0
Dominic <dominic.neu@port-neu.de>
2019-08-27 00:21:43 +0200

    Regression Vorbereitung

--- Testmodell_Masterarbeit/regression.py
+++ Testmodell_Masterarbeit/regression.py
@@ -15,7 +15,7 @@ from calender import get_german_holiday_calendar
 
 DATA_PATH = os.path.join('F:', 'OneDrive', 'Dokumente', '1 Universität - Master', '6. Semester', 'Masterarbeit', 'Implementation', 'Echtdaten')
 
-Artikelstamm = pd.read_csv(
+artikelstamm = pd.read_csv(
     os.path.join(DATA_PATH, '0 ArtikelstammV4.csv'),
     header=0,
     names=['Artikel', 'Warengruppe', 'Detailwarengruppe', 'Bezeichnung',
@@ -26,9 +26,9 @@ Artikelstamm = pd.read_csv(
     memory_map=True
     )
 warengruppen_maske = [1, 12, 55, 80, 17, 77, 71, 6, 28 ]
-Artikelstamm = Artikelstamm[Artikelstamm.Warengruppe.isin(warengruppen_maske)]
-artikel_maske = pd.unique(Artikelstamm.Artikel)
-Artikelstamm = Artikelstamm.set_index('Artikel')
+artikelstamm = artikelstamm[artikelstamm.Warengruppe.isin(warengruppen_maske)]
+artikel_maske = pd.unique(artikelstamm.Artikel)
+# artikelstamm = artikelstamm.set_index('Artikel')
 
 warenausgang = pd.read_csv(
     os.path.join(DATA_PATH, '0 Warenausgang.time.csv'),
@@ -55,95 +55,88 @@ warenausgang.drop(columns=['Artikel'], inplace=True)
 warenausgang.reset_index(inplace=True)
 warenausgang.rename(columns={'level_1': 'Datum'}, inplace=True)
 
-warenausgang.set_index('Artikel', inplace=True)
-"""
-
-Zwischenstand
-
-"""
-
 warenausgang['Wochentag'] = warenausgang.Datum.dt.dayofweek
-# dataframe["Wochentag"] = dataframe["Wochentag"].apply(lambda x: to_categorical(x, num_classes = 7))
 warenausgang['Kalenderwoche'] = warenausgang.Datum.dt.weekofyear
 warenausgang["UNIXDatum"] = warenausgang["Datum"].astype(np.int64)/(1000000000 * 24 * 3600)
 
 mhd_labels = [0, 1, 2, 3, 4, 5, 6]
 mhd_bins = [0, 1, 7, 14, 28, 100, 1000, 100000]
 artikelstamm['MHDgroup'] = pd.cut(artikelstamm.MHD, mhd_bins, right=False, labels=mhd_labels)
-dataframe = pd.merge(dataframe, artikelstamm.loc[:,["Warengruppe", "Einheit", "Eigenmarke", "GuG", "MHDgroup"]], how='left', left_on=["Artikel"], right_index=True)
+warenausgang = pd.merge(warenausgang, artikelstamm.loc[:,["Artikel", "Warengruppe", "Einheit", "Eigenmarke", "GuG", "MHDgroup"]], how='left', on='Artikel', validate='many_to_one')
 
 wetter = pd.read_csv(
-    'F:/OneDrive/Dokumente/1 Universität - Master/6. Semester/Masterarbeit/Implementation/Echtdaten/1 Wetter.csv',
+    os.path.join(DATA_PATH, '1 Wetter.csv'),
     memory_map=True
     )
-wetter["date_shifted_oneday"] = wetter["date"] + 1
-wetter["date_shifted_twodays"] = wetter["date"] + 2
-wetter = wetter.drop(columns=['Unnamed: 0', 'NebenGruppe', 'HauptGruppe'])
-dataframe = pd.merge(dataframe, wetter, left_on='UNIXDatum', right_on='date_shifted_oneday')
-dataframe = pd.merge(dataframe, wetter, left_on='UNIXDatum', right_on='date_shifted_twodays')
-dataframe.drop(columns=["date_shifted_oneday_x", "date_shifted_twodays_x", "date_shifted_oneday_y", "date_shifted_twodays_y"], inplace=True)
+wetter["date_shifted_oneday"] = wetter["date"] - 1
+wetter["date_shifted_twodays"] = wetter["date"] - 2
+wetter.drop(columns=['Unnamed: 0'], inplace=True)
+warenausgang = pd.merge(warenausgang, wetter, left_on='UNIXDatum', right_on='date_shifted_oneday')
+warenausgang = pd.merge(warenausgang, wetter, left_on='UNIXDatum', right_on='date_shifted_twodays', suffixes=('_1D', '_2D'))
+warenausgang.drop(columns=["date_shifted_oneday_1D", "date_shifted_twodays_1D", "date_shifted_oneday_2D", "date_shifted_twodays_2D"], inplace=True)
 
 preise = pd.read_csv(
-    'F:/OneDrive/Dokumente/1 Universität - Master/6. Semester/Masterarbeit/Implementation/Echtdaten/0 RegulärePreise.csv',
+     os.path.join(DATA_PATH, '1 Preise.csv'),
     memory_map=True
     )
-preise = preise.drop(columns=['Unnamed: 0'])
+preise.drop(columns=['Unnamed: 0'], inplace=True)
 preise['Datum'] = pd.to_datetime(preise['Datum'], format='%Y-%m-%d')
 preise.drop_duplicates(inplace=True)
 preise = preise.sort_values(by=['Datum', 'Artikel'])
-preise['Next'] = preise.groupby(['Artikel'])['Datum'].shift(-1)
+preise['Next'] = preise.groupby(['Artikel'], as_index=True)['Datum'].shift(-1)
 preise = preise.where(~preise.isna(), pd.to_datetime('2019-07-01'))
+# preise.set_index('Artikel', inplace=True)
+warenausgang.reset_index(inplace=True)
 
-dataframe = pd.merge_asof(dataframe, preise.loc[:,["Preis", "Artikel", "Datum"]], left_on='Datum', right_on='Datum', by='Artikel')
+warenausgang = pd.merge_asof(warenausgang, preise.loc[:,["Preis", "Artikel", "Datum"]], left_on='Datum', right_on='Datum', by='Artikel')
 
 aktionspreise = pd.read_csv(
-    'F:/OneDrive/Dokumente/1 Universität - Master/6. Semester/Masterarbeit/Implementation/Echtdaten/0 Preisaktionen.csv',
+    os.path.join(DATA_PATH, '1 Preisaktionen.csv'),
     decimal =',',
     memory_map=True
     )
-aktionspreise['DatumAb'] = pd.to_datetime(aktionspreise['DatumAb'], format='%d.%m.%Y')
-aktionspreise['DatumBis'] = pd.to_datetime(aktionspreise['DatumBis'], format='%d.%m.%Y')
-aktionspreise = aktionspreise.sort_values(by=['DatumAb', 'Artikel'])
+aktionspreise.drop(columns=['Unnamed: 0'], inplace=True)
+aktionspreise['DatumAb'] = pd.to_datetime(aktionspreise['DatumAb'], format='%Y-%m-%d')
+aktionspreise['DatumBis'] = pd.to_datetime(aktionspreise['DatumBis'], format='%Y-%m-%d')
 aktionspreise.drop_duplicates(inplace=True)
+aktionspreise = aktionspreise.sort_values(by=['DatumAb', 'DatumBis', 'Artikel'])
 
-aktionspreise = pd.merge_asof(aktionspreise, preise, left_on='DatumAb', right_on='Datum', by='Artikel')
-aktionspreise = aktionspreise.dropna() # Falls für Aktionszeitraum kein regulärer Preis vorhanden => regulärer Preis muss vor Aktionsstart liegen
-aktionspreise = aktionspreise.drop(columns=['Datum', 'Next'])
-aktionspreise['relAbweichung'] = (aktionspreise.Preis - aktionspreise.AktionsPreis)/aktionspreise.Preis
 
-artikel = pd.unique(dataframe.Artikel)
-aktionspreise = aktionspreise[aktionspreise.Artikel.isin(artikel)]
-start = min(dataframe.Datum)
+warenausgang = pd.merge_asof(warenausgang, aktionspreise, left_on='Datum', right_on='DatumAb', by='Artikel')
+warenausgang.relRabat.fillna(0., inplace=True)
+warenausgang.absRabat.fillna(0., inplace=True)
+warenausgang.vDauer.fillna(0, inplace=True)
+warenausgang.relRabat = warenausgang.relRabat.astype(np.float64)
+warenausgang.absRabat = warenausgang.absRabat.astype(np.float64)
+warenausgang.vDauer = warenausgang.vDauer.astype(np.int)
+warenausgang.drop(columns=['DatumAb', 'DatumBis'], inplace=True)
+warenausgang.head(1).T.squeeze()
 
-aktionspreise = aktionspreise[aktionspreise.DatumBis >= start]
-
-# ACHTUNG: Extrem langsam, aber macht Matching einfacher
-#
-# 5 min Laufzeit bei 90.000 Aktionen
-test = aktionspreise.copy()
-test["GroupID"] = test.groupby("Artikel")["DatumBis"].rank()
-test = pd.melt(test, id_vars=['Artikel', 'relAbweichung', 'GroupID'], value_vars=['DatumAb', 'DatumBis'])
-test = test.set_index(['value'])
-drop = lambda df: df.index.drop_duplicates()
-res = lambda df: df[~df.index.duplicated()].resample(rule='1D').ffill()
-# test = test[(test.Artikel==5774) | (test.Artikel==5711)]
-test = test.groupby(['Artikel', 'GroupID'], as_index=False).apply(res)
-test = test.reset_index().drop(["level_0"], axis=1)
-test["Aktion"] = 1
-dataframe = pd.merge(dataframe, test.loc[:,["value","Artikel", "Aktion", "relAbweichung"]], left_on=['Aktion', 'Datum', 'Artikel'], right_on=['Aktion', 'value', 'Artikel'], how='left')
-dataframe.relAbweichung.fillna(0, inplace=True)
-dataframe.drop(columns=["value"], inplace=True)
+warenausgang['in1'] = warenausgang.groupby(['Artikel'], as_index=False)['Menge'].shift(-1)
+warenausgang.in1.fillna(0., inplace=True)
+warenausgang['in2'] = warenausgang.groupby(['Artikel'], as_index=False)['Menge'].shift(-2)
+warenausgang.in2.fillna(0., inplace=True)
+warenausgang['in3'] = warenausgang.groupby(['Artikel'], as_index=False)['Menge'].shift(-3)
+warenausgang.in3.fillna(0., inplace=True)
+warenausgang['in4'] = warenausgang.groupby(['Artikel'], as_index=False)['Menge'].shift(-4)
+warenausgang.in4.fillna(0., inplace=True)
+warenausgang['in5'] = warenausgang.groupby(['Artikel'], as_index=False)['Menge'].shift(-5)
+warenausgang.in5.fillna(0., inplace=True)
 store = pd.HDFStore('./data/time.h5')
 
-store.put('dataframe', dataframe)
+store.put('warenausgang', warenausgang, format='table')
 store.close()
+"""
 
+Zwischenstand
+
+"""
 """ 
 Hier mit fertigen Daten weiterarbeiten 
 """
-store = pd.HDFStore('./data/time.h5')
-dataframe = store.get('dataframe')
-store.close()
-dataframe = dataframe.dropna() # Artikel ohne Preise => nur aktuell ein Problem
+#store = pd.HDFStore('./data/time.h5')
+#dataframe = store.get('dataframe')
+#store.close()
+#dataframe = dataframe.dropna() # Artikel ohne Preise => nur aktuell ein Problem
 
 
