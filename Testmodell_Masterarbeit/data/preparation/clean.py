f973f3fbc6c8879a83db3d736f706ca67d3d9a6d
Dominic <dneu-dilsburg@gmx.de>
2019-10-10 16:53:29 +0200

    Pipeline auf eine Warengruppe umgestellt. Weniger freie Parameter (Zeitraum fix, TS-Länge fix und Markt fix). Datum jetzt als niedrigdimensionales One-Hot-Encoding.
    Signed-off-by: Dominic <dneu-dilsburg@gmx.de>

--- Testmodell_Masterarbeit/data/preparation/clean.py
+++ Testmodell_Masterarbeit/data/preparation/clean.py
@@ -6,6 +6,57 @@ from calender.german_holidays import get_german_holiday_calendar
 from keras.utils import to_categorical
 
 
+def quarters(q):
+    first = int(q/2.5)
+    if 1 < q < 4:
+        second = 1
+    else:
+        second = 0
+    return first, second
+
+
+def quarter_month(m):
+    """ Erstellt ein One-Hot-Encoding für das Quartal"""
+    q_m = m % 3 - 2
+    if q_m == -2:
+        q_m = 1
+    return q_m
+
+
+def week_of_month(date):
+    """ Erstellt One-Hot-Encoding für die Woche des Monats"""
+    months_first = pd.Timestamp(year=date.year, month=date.month, day=1)
+    month_week = date.weekofyear - months_first.weekofyear
+    if month_week == 0:
+        return -1, -1
+    elif month_week == 1:
+        return -1, 0
+    elif month_week == 2:
+        return 0, 0
+    elif month_week == 3:
+        return 0, 1
+    else:
+        return 1, 1
+
+
+def day_of_week(day):
+    """ Erstellt One-Hot-Encoding für den Wochentag"""
+    if day == 0:
+        return -1, -1, -1
+    elif day == 1:
+        return -1, -1, 0
+    elif day == 2:
+        return -1, 0, 0
+    elif day == 3:
+        return 0, 0, 0
+    elif day == 4:
+        return 1, 0, 0
+    elif day == 5:
+        return 1, 1, 0
+    else:
+        return 1, 1, 1
+
+
 def extend_list(list_of_str, length):
     list_copy = list_of_str.copy()
     for i in range(1, length):
@@ -55,7 +106,7 @@ def create_numpy_from_frame(params, absatz, artikelstamm):
     """
     absatz.drop(columns=['Datum'], inplace=True)
     print('INFO - Concatenating dynamic states')
-    lab, dyn, stat_df = concat(absatz, params.ts_length)
+    lab, dyn, stat_df = concat(absatz, 6)
     print('INFO - Reindexing static state')
     stat_df = artikelstamm.reindex(stat_df)
     assert not stat_df.isna().any().any(), 'NaNs im Artikelstamm'
@@ -65,7 +116,7 @@ def create_numpy_from_frame(params, absatz, artikelstamm):
         category_state = to_categorical(stat_df.loc[:, category], num_classes=class_numbers).astype(np.int8)
         stat = np.concatenate((stat, category_state), axis=1)
     print('INFO - Speichere NPZ-Dateien')
-    filename = '-'.join([params.data_group, params.train_start, params.train_stop, str(params.ts_length)])
+    filename = str(params.warengruppenmaske) + ' .npz'
     path = os.path.join(params.output_dir, filename)
     np.savez(path, lab=lab, dyn=dyn, stat=stat)
     return lab, dyn, stat
@@ -96,9 +147,63 @@ def create_frame_from_raw_data(params):
     artikelmaske = pd.unique(artikelstamm.Artikel)
     # endregion
 
+    # region Warenbewegung
+    warenausgang = pd.read_csv(
+        os.path.join(params.input_dir, '0 Warenausgang.Markt.csv'),
+        header=1,
+        names=['Markt', 'Artikel', 'Belegtyp', 'Menge', 'Datum']
+    )
+    warenausgang = warenausgang[warenausgang.Artikel.isin(artikelmaske)]
+    warenausgang['Datum'] = pd.to_datetime(warenausgang['Datum'], format='%d.%m.%y')
+    absatz = warenausgang[warenausgang.Belegtyp.isin(['UMSATZ_SCANNING', 'UMSATZ_AKTION'])]
+    absatz = absatz.groupby(['Markt', 'Artikel', 'Datum'], as_index=False).sum()
+
+    # Verwerfe Artikel, die nicht jedes Jahr verkauft wurden
+    absatz['Jahr'] = absatz.Datum.dt.year
+    absatz_jahre = absatz.groupby(['Artikel', 'Markt'])['Jahr'].nunique()
+    max_abs_jahre = max(absatz_jahre)
+    artikel_mit_durchgehendem_abs = absatz_jahre[absatz_jahre == max_abs_jahre].index.values
+    absatz.set_index(['Artikel', 'Markt'], drop=False, inplace=True)
+    absatz = absatz.loc[artikel_mit_durchgehendem_abs]
+    absatz.reset_index(drop=True, inplace=True)
+    # Verwerfe Artikel, die seltener als an 10 Tage p.a. verkauft werden.
+    absatz_vorgaenge = absatz.groupby(['Artikel', 'Jahr', 'Markt'])['Menge'].count()
+    art_abs_unter_10_art = absatz_vorgaenge[absatz_vorgaenge > 10].index.get_level_values('Artikel').values
+    art_abs_unter_10_mkt = absatz_vorgaenge[absatz_vorgaenge > 10].index.get_level_values('Markt').values
+    art_abs_unter_10 = [(art, mkt) for art, mkt in zip(art_abs_unter_10_art, art_abs_unter_10_mkt)]
+    art_abs_unter_10 = set(art_abs_unter_10)
+    absatz.set_index(['Artikel', 'Markt'], drop=False, inplace=True)
+    absatz = absatz.loc[list(art_abs_unter_10)]
+    absatz.reset_index(drop=True, inplace=True)
+
+    # Artikelmaske auf relevante beschränken
+    artikelmaske = absatz.Artikel.unique()
+    warenausgang = warenausgang[warenausgang.Artikel.isin(artikelmaske)]
+    artikelstamm = artikelstamm[artikelstamm.Artikel.isin(artikelmaske)]
+
+    wareneingang = pd.read_csv(
+        os.path.join(params.input_dir, '0 Wareneingang.Markt.csv'),
+        header=1,
+        names=['Markt', 'Artikel', 'Belegtyp', 'Menge', 'Datum']
+    )
+    wareneingang = wareneingang[wareneingang.Artikel.isin(artikelmaske)]
+    wareneingang['Datum'] = pd.to_datetime(wareneingang['Datum'], format='%d.%m.%y')
+
+    bestand = pd.read_csv(
+        os.path.join(params.input_dir, '0 Warenbestand.Markt.csv'),
+        header=1,
+        names=['Markt', 'Artikel', 'Bestand', 'EK', 'VK', 'Anfangsbestand', 'Datum']
+    )
+    bestand['Datum'] = pd.to_datetime(bestand['Datum'], format='%d.%m.%y')
+
+    warenausgang['Menge'] = -warenausgang['Menge']
+
+    bewegung = pd.concat([warenausgang, wareneingang])
+    # endregion
+
     # region Preise
     preise = pd.read_csv(
-        os.path.join(params.input_dir, '0 Preise.' + params.data_group + '.csv'),
+        os.path.join(params.input_dir, '0 Preise.Markt.csv'),
         header=1,
         names=['Preis', 'Artikel', 'Datum']
     )
@@ -111,45 +216,13 @@ def create_frame_from_raw_data(params):
         preise['Markt'] = 5
 
     aktionspreise = pd.read_csv(
-        os.path.join(params.input_dir, '0 Aktionspreise.' + params.data_group + '.csv'),
+        os.path.join(params.input_dir, '0 Aktionspreise.Markt.csv'),
         header=1,
         names=['Artikel', 'DatumAb', 'DatumBis', 'Aktionspreis']
     )
     aktionspreise = aktionspreise[aktionspreise.Artikel.isin(artikelmaske)]
     aktionspreise['DatumAb'] = pd.to_datetime(aktionspreise['DatumAb'], format='%d.%m.%Y')
     aktionspreise['DatumBis'] = pd.to_datetime(aktionspreise['DatumBis'], format='%d.%m.%Y')
-    # endregion
-
-    # region Warenbewegung
-    warenausgang = pd.read_csv(
-        os.path.join(params.input_dir, '0 Warenausgang.' + params.data_group + '.csv'),
-        header=1,
-        names=['Markt', 'Artikel', 'Belegtyp', 'Menge', 'Datum']
-    )
-    warenausgang = warenausgang[warenausgang.Artikel.isin(artikelmaske)]
-    warenausgang['Datum'] = pd.to_datetime(warenausgang['Datum'], format='%d.%m.%y')
-
-    wareneingang = pd.read_csv(
-        os.path.join(params.input_dir, '0 Wareneingang.' + params.data_group + '.csv'),
-        header=1,
-        names=['Markt', 'Artikel', 'Belegtyp', 'Menge', 'Datum']
-    )
-    wareneingang = wareneingang[wareneingang.Artikel.isin(artikelmaske)]
-    wareneingang['Datum'] = pd.to_datetime(wareneingang['Datum'], format='%d.%m.%y')
-
-    bestand = pd.read_csv(
-        os.path.join(params.input_dir, '0 Warenbestand.' + params.data_group + '.csv'),
-        header=1,
-        names=['Markt', 'Artikel', 'Bestand', 'EK', 'VK', 'Anfangsbestand', 'Datum']
-    )
-    bestand['Datum'] = pd.to_datetime(bestand['Datum'], format='%d.%m.%y')
-
-    absatz = warenausgang[warenausgang.Belegtyp.isin(['UMSATZ_SCANNING', 'UMSATZ_AKTION'])]
-    absatz = absatz.groupby(['Markt', 'Artikel', 'Datum'], as_index=False).sum()
-
-    warenausgang['Menge'] = -warenausgang['Menge']
-
-    bewegung = pd.concat([warenausgang, wareneingang])
     # endregion
 
     # region Wetter
@@ -201,13 +274,7 @@ def create_frame_from_raw_data(params):
         'Warengruppe': warengruppen_index,
         'Einheit': einheit_index
     }
-    filename = '-'.join([
-        params.data_group,
-        params.train_start,
-        params.train_stop,
-        str(params.ts_length),
-        'ValueMapping.json'
-    ])
+    filename = str(params.warengruppenmaske) + ' ValueMapping.json'
     with open(os.path.join(params.output_dir, filename), 'w') as file:
         json.dump(mapping, file)
     artikelstamm['Detailwarengruppe'] = artikelstamm['Detailwarengruppe'].map(
@@ -227,8 +294,8 @@ def create_frame_from_raw_data(params):
     cal = cal_cls()
     sl_bd = pd.tseries.offsets.CustomBusinessDay(calendar=cal, weekmask='Mon Tue Wed Tue Fri Sat')
     zeitraum = pd.date_range(
-        pd.to_datetime(params.train_start),
-        pd.to_datetime(params.train_stop) + pd.DateOffset(7),
+        pd.to_datetime('2018-01-01'),
+        pd.to_datetime('2019-07-01') + pd.DateOffset(7),
         freq=sl_bd
     )
     absatz.set_index('Datum', inplace=True)
@@ -237,8 +304,14 @@ def create_frame_from_raw_data(params):
     absatz.reset_index(inplace=True)
     absatz.rename(columns={'level_2': 'Datum'}, inplace=True)
 
-    absatz['Wochentag'] = absatz.Datum.dt.dayofweek
-    absatz['Kalenderwoche'] = absatz.Datum.dt.weekofyear
+    absatz['j'] = absatz.Datum.dt.year - 2018
+    absatz['q1'], absatz['q2'] = zip(*absatz.Datum.dt.quarter.apply(quarters))
+    absatz['q_m'] = absatz.Datum.dt.quarter.apply(quarter_month)
+    absatz['w1'], absatz['w2'] = zip(*absatz.Datum.apply(week_of_month))
+    absatz['t1'], absatz['t2'], absatz['t3'] = zip(*absatz.Datum.dt.dayofweek.apply(day_of_week))
+
+    # absatz['Wochentag'] = absatz.Datum.dt.dayofweek
+    # absatz['Kalenderwoche'] = absatz.Datum.dt.weekofyear
     absatz["UNIXDatum"] = absatz["Datum"].astype(np.int64) / (1000000000 * 24 * 3600)
     # endregion
 
@@ -296,13 +369,7 @@ def create_frame_from_raw_data(params):
     print('{:.2f} % der Daten aufgrund fehlender Preise verworfen.'.format(np.mean(absatz.Preis.isna()) * 100))
     preis_mean, preis_std = np.mean(absatz.Preis), np.std(absatz.Preis)
     absatz['Preis'] = (absatz['Preis'] - preis_mean) / preis_std
-    filename = '-'.join([
-        params.data_group,
-        params.train_start,
-        params.train_stop,
-        str(params.ts_length),
-        'PreisStd.json'
-    ])
+    filename = str(params.warengruppenmaske) + ' PreisStd.json'
     with open(os.path.join(params.output_dir, filename), 'w') as file:
         json.dump({'PreisStandardDerivation': preis_std, 'PreisMean': preis_mean}, file)
     absatz.dropna(inplace=True)
@@ -334,11 +401,17 @@ def create_frame_from_raw_data(params):
     absatz['in3'] = absatz.groupby(['Markt', 'Artikel'], as_index=False)['Menge'].shift(-3)
     absatz['in4'] = absatz.groupby(['Markt', 'Artikel'], as_index=False)['Menge'].shift(-4)
     absatz['in5'] = absatz.groupby(['Markt', 'Artikel'], as_index=False)['Menge'].shift(-5)
+    absatz['in6'] = absatz.groupby(['Markt', 'Artikel'], as_index=False)['Menge'].shift(-6)
     absatz.dropna(axis=0, inplace=True)
     absatz.sort_values(['Markt', 'Artikel', 'Datum'], inplace=True)
     # endregion
-    # TODO:Bestand und weitere Stammdaten für Statistiken zurückgeben
-    # TODO: Fertige Frames in HDF-Store ablegen
+    filename = str(params.warengruppenmaske) + ' store.h5'
+    store = pd.HDFStore(os.path.join(params.output_dir, filename))
+    store.put('Artikelstamm', artikelstamm)
+    store.put('Absatz', absatz)
+    store.put('Bewegung', bewegung)
+    store.close()
+    # TODO: Bestand und weitere Stammdaten für Statistiken zurückgeben
     # TODO: Checken, ob das Updaten der Dicts zu kategorialen Variable Einfluss auf den Dateinamen hat.
     return absatz, bewegung, artikelstamm
 
