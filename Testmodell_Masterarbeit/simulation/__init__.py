9761a716ade74efa53bd1ce27f00830bbffdf229
Dominic Neu <dneu-dilsburg@gmx.de>
2019-11-06 19:41:21 +0100

    Bestandsreichweite Version 2
    Signed-off-by: Dominic <dneu-dilsburg@gmx.de>

--- Testmodell_Masterarbeit/simulation/__init__.py
+++ Testmodell_Masterarbeit/simulation/__init__.py
@@ -5,6 +5,58 @@ import matplotlib.pyplot as plt
 from scipy.stats import entropy
 
 
+def belohnung_bestandsreichweite(_bestand, _absatz, order_zyklus, rohertrag=0.3, ek_preis=0.7,
+                                 kap_kosten=0.05 / 365):
+    """
+    Berechnet die Belohnung für den kommenden Bestand
+    """
+    assert len(_absatz) > 1
+    analyse_tage = len(_absatz)
+    anfangsbestand = _bestand.shape[0]
+    kum_absatz = _absatz.cumsum()
+    tage, mengen = np.unique(_bestand, return_counts=True)
+    abschr_bestand = np.zeros(analyse_tage, dtype=np.int64)
+    for tag, menge in zip(tage, mengen):
+        abschr_bestand[:tag] += menge
+
+    absatz_bestand = anfangsbestand - kum_absatz
+    abschriften = np.clip(abschr_bestand - absatz_bestand, None, 0)
+
+    real_bestand = absatz_bestand + abschriften
+
+    break_even = np.argwhere(real_bestand < 0)
+    if break_even.size > 0:
+        bestandsreichweite = break_even[0, 0]
+    else:
+        bestandsreichweite = analyse_tage - 1
+    anz_abschriften = -abschriften[:bestandsreichweite].sum()
+    absatz_ausfall = _absatz[bestandsreichweite + 1:].sum()
+    # Sonderanalyse Tag des Umbruchs
+    tages_absatz = _absatz[bestandsreichweite]
+    vor_tages_bestand = real_bestand[bestandsreichweite - 1]
+    tages_abschrift = abschr_bestand[bestandsreichweite - 1] - abschr_bestand[bestandsreichweite]
+
+    # Annahme, das Abschrift zuerst eintritt
+    restmenge_nach_abschrift = vor_tages_bestand - tages_abschrift
+    if restmenge_nach_abschrift > 0:  # Nach Abschrift noch Artikel im Bestand
+        anz_abschriften += tages_abschrift
+        absatz_ausfall += max(tages_absatz - restmenge_nach_abschrift, 0)
+    else:
+        anz_abschriften += vor_tages_bestand  # Wenn Restmenge negativ, kann nur der Bestand abgeschrieben werden
+        absatz_ausfall += tages_absatz  # Absatz fällt komplett aus
+
+    rew_abschrift = anz_abschriften * - ek_preis
+    if bestandsreichweite > order_zyklus - 1:
+        rew_bestand = - kap_kosten * real_bestand[order_zyklus]
+        rew_fehlmenge = 0
+    else:
+        rew_bestand = 0
+        fehlmenge = absatz_ausfall - _absatz[order_zyklus:].sum()
+        rew_fehlmenge = fehlmenge * - rohertrag
+
+    return np.array([rew_abschrift, rew_bestand, rew_fehlmenge])
+
+
 class Statistics(object):
     def __init__(self):
         self.data = {}
@@ -280,12 +332,27 @@ class StockSimulation(object):
                     reward = 0.1
             elif reichweite > 0:
                 # Korrigierbarer Überbestand?
-                # TODO: Schauen ob Überbestand durch weniger Bestellmenge an den Folgetagen ausgeglichen werden kann
                 reward = reichweite * - 0.1
             else:
                 reward = reichweite * 0.3  # fürs Erste fixe Bestrafung
 
             reward = np.clip(reward, -3, 3)
+
+        elif self.reward_flag == 'Bestandsreichweite V2':
+            analyse_start = self.vergangene_tage+1
+            analyse_stop = min(self.tage, analyse_start + 2 * self.bestellrythmus)
+            kommende_absaetze = np.sum(
+                self.artikel_absatz[analyse_start:analyse_stop]
+            )
+            rewards = belohnung_bestandsreichweite(
+                self.bestands_frische,
+                kommende_absaetze,
+                self.bestellrythmus,
+                rohertrag=0.3,
+                ek_preis=0.7,
+                kap_kosten=0.05 / 365
+            )
+            reward = np.clip(rewards.sum(), -3, 3)
 
         else:
             raise NotImplementedError('Unbekannte Belohnungsart')
--- Testmodell_Masterarbeit/testing.py
+++ Testmodell_Masterarbeit/testing.py
@@ -5,10 +5,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def belohnung_bestandsreichweite(_bestand, _absatz, order_zyklus, rohertrag=0.3, ek_preis=0.7, kap_kosten=0.05/365):
+    """
+    Berechnet die Belohnung für den kommenden Bestand
+    """
+    assert len(_absatz) > 1
+    analyse_tage = len(_absatz)
+    anfangsbestand = _bestand.shape[0]
+    kum_absatz = _absatz.cumsum()
+    tage, mengen = np.unique(_bestand, return_counts=True)
+    abschr_bestand = np.zeros(analyse_tage, dtype=np.int64)
+    for tag, menge in zip(tage, mengen):
+        abschr_bestand[:tag] += menge
+
+    absatz_bestand = anfangsbestand - kum_absatz
+    abschriften = np.clip(abschr_bestand - absatz_bestand, None, 0)
+
+    real_bestand = absatz_bestand + abschriften
+
+    break_even = np.argwhere(real_bestand < 0)
+    if break_even.size > 0:
+        bestandsreichweite = break_even[0, 0]
+    else:
+        bestandsreichweite = analyse_tage - 1
+    anz_abschriften = -abschriften[:bestandsreichweite].sum()
+    absatz_ausfall = _absatz[bestandsreichweite + 1:].sum()
+    # Sonderanalyse Tag des Umbruchs
+    tages_absatz = _absatz[bestandsreichweite]
+    vor_tages_bestand = real_bestand[bestandsreichweite - 1]
+    tages_abschrift = abschr_bestand[bestandsreichweite - 1] - abschr_bestand[bestandsreichweite]
+
+    # Annahme, das Abschrift zuerst eintritt
+    restmenge_nach_abschrift = vor_tages_bestand - tages_abschrift
+    if restmenge_nach_abschrift > 0:  # Nach Abschrift noch Artikel im Bestand
+        anz_abschriften += tages_abschrift
+        absatz_ausfall += max(tages_absatz - restmenge_nach_abschrift, 0)
+    else:
+        anz_abschriften += vor_tages_bestand  # Wenn Restmenge negativ, kann nur der Bestand abgeschrieben werden
+        absatz_ausfall += tages_absatz  # Absatz fällt komplett aus
+
+    rew_abschrift = anz_abschriften * - ek_preis
+    if bestandsreichweite > order_zyklus - 1:
+        rew_bestand = - kap_kosten * real_bestand[order_zyklus]
+        rew_fehlmenge = 0
+    else:
+        rew_bestand = 0
+        fehlmenge = absatz_ausfall - _absatz[order_zyklus:].sum()
+        rew_fehlmenge = fehlmenge * - rohertrag
+
+    return np.array([rew_abschrift, rew_bestand, rew_fehlmenge])
+
+
 bestand = np.array([
-    3,
-    3,
-    4,
+    2,
+    2,
+    2,
     14,
     14,
     14,
@@ -16,39 +67,31 @@ bestand = np.array([
     14
 ])
 absatz = np.array([
-    8,
+    1,
+    1,
+    0,
+    3,
+    2,
+    1,
 ])
-kum_absatz = absatz.cumsum()
-bestand_max_reichweite = bestand[-1]
-tage, mengen = np.unique(bestand, return_counts=True)
-cum_bestand = np.zeros(kum_absatz.shape, dtype=np.int64)  # brauche den Bestand für die zu betrachtende Absatzperiode
-for tag, menge in zip(tage, mengen):
-    cum_bestand[:tag] += menge
-    print(cum_bestand)
-print('diff', (cum_bestand - kum_absatz))
-print(np.argwhere((cum_bestand - kum_absatz) < 0))
-break_even = np.argwhere((cum_bestand - kum_absatz) < 0)
-if break_even.size > 0:
-    bestandsreichweite = break_even[0, 0]
-else:
-    bestandsreichweite = len(absatz) - 1
-abschriften = cum_bestand[0] - cum_bestand[bestandsreichweite-1]
-absatz_ausfall = absatz[bestandsreichweite + 1:].sum()
-# Sonderanalyse Tag des Umbruchs
-tages_absatz = absatz[bestandsreichweite]
-vor_tages_bestand = cum_bestand[bestandsreichweite - 1] - kum_absatz[bestandsreichweite - 1]
-tages_abschrift = cum_bestand[bestandsreichweite - 1] - cum_bestand[bestandsreichweite]
+reward = belohnung_bestandsreichweite(bestand, absatz, 3)
 
-# Annahme, das Abschrift zuerst eintritt
-restmenge_nach_abschrift = vor_tages_bestand - tages_abschrift
-if restmenge_nach_abschrift > 0:  # Nach Abschrift noch Artikel im Bestand
-    abschriften += tages_abschrift
-    absatz_ausfall += max(tages_absatz - restmenge_nach_abschrift, 0)
-else:
-    abschriften += vor_tages_bestand  # Wenn Restmenge negativ, kann nur der Bestand abgeschrieben werden
-    absatz_ausfall += tages_absatz  # Absatz fällt komplett aus
-
-plt.plot(cum_bestand, label='Bestand')
-plt.plot(kum_absatz, label='Absatz')
-plt.legend()
-plt.show()
+# kum_absatz = absatz.cumsum()
+# tage, mengen = np.unique(bestand, return_counts=True)
+# abschr_bestand = np.zeros(kum_absatz.shape,
+#                           dtype=np.int64)  # brauche den Bestand für die zu betrachtende Absatzperiode
+# for tag, menge in zip(tage, mengen):
+#     abschr_bestand[:tag] += menge
+#
+# absatz_bestand = 8 - kum_absatz
+# abschriften = np.clip(abschr_bestand - absatz_bestand, None, 0)
+#
+# real_bestand = absatz_bestand + abschriften
+#
+# plt.bar(np.arange(0, 6) - 0.375, abschr_bestand, width=1/4, fill=True, label='Abschriftsbestand', color='red', alpha=0.3)
+# plt.bar(np.arange(0, 6) - 0.125, absatz_bestand, width=1/4, fill=True, label='Absatzbestand', color='blue', alpha=0.3)
+# plt.bar(np.arange(0, 6) + 0.125, abschriften, width=1/4, fill=True, label='Wahre Abschriften', color='green', alpha=0.3)
+# plt.bar(np.arange(0, 6) + 0.375, real_bestand, width=1/4, fill=True, label='Realbestand', color='orange', alpha=0.3)
+#
+# plt.legend()
+# plt.show()
