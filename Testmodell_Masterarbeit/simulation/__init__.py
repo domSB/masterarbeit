d97d053dd4f336e4f52dcf1c39a077a52a4ffd28
Dominic <dneu-dilsburg@gmx.de>
2019-11-07 12:48:30 +0100

    Belohnungsfunktion mal wieder umgebaut
    Signed-off-by: Dominic <dneu-dilsburg@gmx.de>

--- Testmodell_Masterarbeit/simulation/__init__.py
+++ Testmodell_Masterarbeit/simulation/__init__.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 from scipy.stats import entropy
 
 
-def belohnung_bestandsreichweite(_bestand, _absatz, order_zyklus, rohertrag=0.3, ek_preis=0.7,
+def belohnung_bestandsreichweite(bestand, absatz, order_zyklus, rohertrag=0.3, ek_preis=0.7,
                                  kap_kosten=0.05 / 365):
     """
     Berechnet die Belohnung für den kommenden Bestand
     """
-    assert len(_absatz) > 1
-    analyse_tage = len(_absatz)
-    anfangsbestand = _bestand.shape[0]
-    kum_absatz = _absatz.cumsum()
-    tage, mengen = np.unique(_bestand, return_counts=True)
+    assert len(absatz) > 1, "Belohnungsfunktion benötigt mehr als einen Absatztag für den Horizont."
+    analyse_tage = len(absatz)
+    anfangsbestand = bestand.shape[0]
+    kum_absatz = absatz.cumsum()
+    tage, mengen = np.unique(bestand, return_counts=True)
     abschr_bestand = np.zeros(analyse_tage, dtype=np.int64)
     for tag, menge in zip(tage, mengen):
         abschr_bestand[:tag] += menge
@@ -28,37 +28,37 @@ def belohnung_bestandsreichweite(_bestand, _absatz, order_zyklus, rohertrag=0.3,
     if break_even.size > 0:
         bestandsreichweite = break_even[0, 0]
     else:
-        bestandsreichweite = analyse_tage - 1
-    anz_abschriften = -abschriften[:bestandsreichweite].sum()
-    absatz_ausfall = _absatz[bestandsreichweite + 1:].sum()
-    # Sonderanalyse Tag des Umbruchs
-    tages_absatz = _absatz[bestandsreichweite]
-    vor_tages_bestand = real_bestand[bestandsreichweite - 1]
-    tages_abschrift = abschr_bestand[bestandsreichweite - 1] - abschr_bestand[bestandsreichweite]
+        bestandsreichweite = analyse_tage
 
-    # Annahme, das Abschrift zuerst eintritt
-    restmenge_nach_abschrift = vor_tages_bestand - tages_abschrift
-    if restmenge_nach_abschrift > 0:  # Nach Abschrift noch Artikel im Bestand
-        anz_abschriften += tages_abschrift
-        absatz_ausfall += max(tages_absatz - restmenge_nach_abschrift, 0)
+    # Fallunterscheidungen:
+    # 1. Fall Bestandsreichweite == Orderzyklus
+    # ==> Perfekter Bestand
+    if bestandsreichweite == order_zyklus:
+        reward = 0.1 + 0.3 * kum_absatz[order_zyklus - 1]**2
+        # Exponentielle steigende Belohnung für Treffen von hohen Absätzen
+
+    # 2. Fall Bestandsreichweite < Orderzyklus
+    # ==> Unterbestand mit Fehlmenge
+    elif bestandsreichweite < order_zyklus:
+        fehlmenge = real_bestand[order_zyklus - 1]
+        reward = -rohertrag * fehlmenge**2
+
+    # 3. Fall Bestandsreichweite > Orderzyklus & Bestand bei Orderzyklus == 0
+    # ==> Bestandsreichweite per Definition höher als Orderzyklus, aber Bestellmenge optimal
     else:
-        anz_abschriften += vor_tages_bestand  # Wenn Restmenge negativ, kann nur der Bestand abgeschrieben werden
-        absatz_ausfall += tages_absatz  # Absatz fällt komplett aus
+        end_bestand = real_bestand[order_zyklus - 1]
+        if end_bestand == 0:
+            reward = 0.1 + 0.3 * kum_absatz[order_zyklus - 1]**2
 
-    rew_abschrift = anz_abschriften * - ek_preis
-    if bestandsreichweite > order_zyklus:
-        rew_bestand = - kap_kosten * real_bestand[order_zyklus - 1] * ek_preis
-        rew_fehlmenge = 0
-    elif bestandsreichweite == order_zyklus:
-        rew_fehlmenge = rohertrag * _absatz[:order_zyklus+1].sum()  # Belohnung, wenn richtige Menge getroffen
-        rew_bestand = 0
-    else:
-        rew_bestand = 0
-        fehlmenge = absatz_ausfall - _absatz[order_zyklus:].sum()
-        rew_fehlmenge = fehlmenge * - rohertrag
+    # 4. Fall Bestandsreichweite > Orderzyklus & Bestand bei Orderzyklus > 0
+    # ==> Noch Bestand bei nächstem Liefereingang.
+        else:
+            unvermeidbare_abschriften = -abschriften[order_zyklus-1:].sum()
+            # Bei MHD > 2x Orderzyklus werden Abschriften ggf. mehrfach bestraft.
+            verkaufbarer_mehrbestand = end_bestand - unvermeidbare_abschriften
+            reward = (unvermeidbare_abschriften * -ek_preis) + (verkaufbarer_mehrbestand * -ek_preis * kap_kosten)
 
-    return np.array([rew_abschrift, rew_bestand, rew_fehlmenge])
-
+    return reward
 
 class Statistics(object):
     def __init__(self):
